20ce8c8ffd2a716d52aa8025e3171e17c7573899
Marina Ryzhikova~ <marina.ryzhikova2711@yandex.ru>
2022-10-31 19:57:33 +0300

    задание 2.2

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-echo 'Номер строки __LINE__';
-echo __FILE__;
+echo __LINE__ . PHP_EOL;
+echo __FILE__ . PHP_EOL;
+
+$newvariable = <<<END
+    1 строка
+        2 строка 
+        3 строка
+END;
+
+echo $newvariable;
+
+$a='Рыба';
+$b='человек';
+echo "$a рыбою сыта, а $b человеком";
+
